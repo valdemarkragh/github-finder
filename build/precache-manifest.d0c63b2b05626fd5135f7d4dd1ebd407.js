b0cd30f09c7271e73edc79e9759ac4c812f56246
DESKTOP-0I5NDE7\Valdemar <valdemar.kragh@hotmail.com>
2019-06-22 22:26:22 +0200

    first commit

---
+++ build/precache-manifest.d0c63b2b05626fd5135f7d4dd1ebd407.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30a29bf62269139f3ab5cf1e6ee0cff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0d017668431b8f95869a",
+    "url": "/static/css/main.65a3f7db.chunk.css"
+  },
+  {
+    "revision": "15d01be9aa2676e13cdf",
+    "url": "/static/js/2.d8131e92.chunk.js"
+  },
+  {
+    "revision": "0d017668431b8f95869a",
+    "url": "/static/js/main.0e5670c2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
